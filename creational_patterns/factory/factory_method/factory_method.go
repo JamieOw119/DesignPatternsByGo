846d225bc40f4bf37803534e2fa6a6061fbbb422
JamieOw119 <1139937387@qq.com>
2021-07-30 14:45:08 +0800

    factory

---
+++ creational_patterns/factory/factory_method/factory_method.go
@@ -0,0 +1,47 @@
+package factory
+
+type IRuleConfigParser interface {
+	Parse(data []byte)
+}
+
+type jsonRuleConfigParser struct {
+}
+
+func (J jsonRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+type yamlRuleConfigParser struct {
+}
+
+func (Y yamlRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+type IRuleConfigParserFactory interface {
+	CreateParser() IRuleConfigParser
+}
+
+type yamlRuleConfigParserFactory struct {
+}
+
+func (y yamlRuleConfigParserFactory) CreateParser() IRuleConfigParser {
+	return yamlRuleConfigParser{}
+}
+
+type jsonRuleConfigParserFactory struct {
+}
+
+func (j jsonRuleConfigParserFactory) CreateParser() IRuleConfigParser {
+	return jsonRuleConfigParser{}
+}
+
+func NewIRuleConfigParserFactory(t string) IRuleConfigParserFactory {
+	switch t {
+	case "json":
+		return jsonRuleConfigParserFactory{}
+	case "yaml":
+		return yamlRuleConfigParserFactory{}
+	}
+	return nil
+}
